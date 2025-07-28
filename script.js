c2e47a6066ba64f9e3d5b87b9883c81ba407945f
Kanga <iamkanga@gmail.com>
2025-07-29 05:21:23 +0700

    htd

--- script.js
+++ script.js
@@ -2560,7 +2560,11 @@ function renderWatchlist() {
 
             targetHitItem.addEventListener('click', () => {
                 const clickedShareId = targetHitItem.dataset.shareId;
-                if (clickedShareId) {…}
+                if (clickedShareId) {
+                    hideModal(targetHitDetailsModal);
+                    selectShare(clickedShareId);
+                    showShareDetails();
+                }
             });
         });
     }
